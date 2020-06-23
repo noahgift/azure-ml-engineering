6874f723a3f5a06b63f302cd27c447bd6400306a
Noah Gift <noah.gift@gmail.com>
2020-06-23 15:05:07 +0000

    more upgrades

---
+++ ingest.py
@@ -0,0 +1,15 @@
+from azureml.opendatasets import NycTlcGreen
+import pandas as pd
+from datetime import datetime
+from dateutil.relativedelta import relativedelta
+
+green_taxi_df = pd.DataFrame([])
+start = datetime.strptime("1/1/2015","%m/%d/%Y")
+end = datetime.strptime("1/31/2015","%m/%d/%Y")
+
+for sample_month in range(12):
+    temp_df_green = NycTlcGreen(start + relativedelta(months=sample_month), end + relativedelta(months=sample_month)) \
+        .to_pandas_dataframe()
+    green_taxi_df = green_taxi_df.append(temp_df_green.sample(2000))
+
+green_taxi_df.head(10)
